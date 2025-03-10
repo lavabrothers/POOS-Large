d3a006512b45507e580b5f4f2ec550ad7c93c6c6
RubenDennis <145509659+RubenDennis@users.noreply.github.com>
2025-03-10 15:18:14 -0400

    Create SignUpPage.tsx

---
+++ frontend/src/pages/SignUpPage.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import PageTitle from '../components/PageTitle.tsx';
+import SignUp from '../components/SignUp.tsx';
+
+const SignUpPage = () => {
+  return (
+    <div>
+      <PageTitle />
+      <SignUp />
+    </div>
+  );
+};
+
+export default SignUpPage;
