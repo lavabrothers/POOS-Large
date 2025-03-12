32b9be4227f8e8b5e38f915ac2a1131f2f9fed2e
RubenDennis <145509659+RubenDennis@users.noreply.github.com>
2025-03-12 13:27:59 -0400

    Update SignUp.tsx

--- frontend/src/components/SignUp.tsx
+++ frontend/src/components/SignUp.tsx
@@ -1,109 +1,106 @@
 import React, { useState } from 'react';
 
-function SignUp() {
-  const [message, setMessage] = useState('');
-  const [firstName, setFirstName] = useState('');
-  const [lastName, setLastName] = useState('');
-  const [username, setUsername] = useState('');
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+function SignUp() 
+{
+const [message, setMessage] = useState('');
+const [firstName, setFirstName] = React.useState('');
+const [lastName, setLastName] = React.useState('');
+const [loginEmail, setEmail] = React.useState('');
+const [loginName, setLoginName] = React.useState('');
+const [loginPassword, setPassword] = React.useState('');
 
-  async function doSignUp(e: React.FormEvent) {
-    e.preventDefault();
+function handleSetFirstName( e: any ) : void
+{
+  setFirstName( e.target.value );
+}
+function handleSetLastName( e: any ) : void
+{
+  setLastName( e.target.value );
+}
 
-    const obj = {
-      username: username,
-      email: email,
-      password: password,
-      firstName: firstName,
-      lastName: lastName
-    };
+function handleSetEmail( e: any ) : void
+{
+  setLastName( e.target.value );
+}
+function handleSetLoginName( e: any ) : void
+{
+  setLoginName( e.target.value );
+}
 
-    try {
-      const response = await fetch('http://134.122.3.46:3000/api/signup', {
-        method: 'POST',
-        body: JSON.stringify(obj),
-        headers: { 'Content-Type': 'application/json' },
-      });
+function handleSetPassword( e: any ) : void
+{
+  setPassword( e.target.value );
+}
 
-      const res = await response.json();
+function goToOnBoard() : void 
+{
+ 
+  window.location.href = '/onboard';
+}
 
-      // Adjust the logic based on your backend’s actual response
-      if (!res || res.id <= 0) {
-        setMessage('Unable to create user. Username or email may already be taken.');
-      } else {
-        // Example: store user data in localStorage, then redirect
-        const user = {
-          firstName: res.firstName,
-          lastName: res.lastName,
-          id: res.id
-        };
-        localStorage.setItem('user_data', JSON.stringify(user));
+async function doSignUp(event:any) : Promise<void>
+{
+  event.preventDefault();
 
-        setMessage('');
-        window.location.href = '/cards';
-      }
-    } catch (error: any) {
-      setMessage(error.toString());
+  var obj = {firstname: firstName, lastname: lastName , email: loginEmail, username: loginName, password: loginPassword,};
+  // obj { Ruben, Dennis, rubendennis@gmail.com, username1, password1}
+
+  var js = JSON.stringify(obj);
+
+  try
+  {    
+
+    const response = await fetch('http://134.122.3.46:3000/api/signup', {
+        method:'POST',
+        body: js,
+        headers:{'Content-Type': 'application/json'}});
+        
+    var res = JSON.parse(await response.text());
+
+
+    if ("Invalid username or password." == res.error)
+    {
+      setMessage('User/Password combination incorrect');
+    }
+    else if ("Username and password are required." == res.error) {
+      setMessage('Username and password are required.')
+    }
+    else
+    {
+      var user = res.user
+      localStorage.setItem('user_data', JSON.stringify(user));
+
+      setMessage('Login successful!');
+      //window.location.href = '/cards';
     }
   }
+  catch(error:any)
+  {
+      alert(error.toString());
+      return;
+  }    
+};
 
-  return (
-    <div id="signupDiv">
-      <span id="inner-title">CREATE AN ACCOUNT</span>
-      <br />
-
-      <input
-        type="text"
-        placeholder="First Name"
-        value={firstName}
-        onChange={(e) => setFirstName(e.target.value)}
-      />
-      <br />
-
-      <input
-        type="text"
-        placeholder="Last Name"
-        value={lastName}
-        onChange={(e) => setLastName(e.target.value)}
-      />
-      <br />
-
-      <input
-        type="text"
-        placeholder="Username"
-        value={username}
-        onChange={(e) => setUsername(e.target.value)}
-      />
-      <br />
-
-      <input
-        type="email"
-        placeholder="Email"
-        value={email}
-        onChange={(e) => setEmail(e.target.value)}
-      />
-      <br />
-
-      <input
-        type="password"
-        placeholder="Password"
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
-      />
-      <br />
-
-      <input
-        type="submit"
-        id="signupButton"
-        className="buttons"
-        value="Sign Up"
-        onClick={doSignUp}
-      />
-
-      <span id="signupResult">{message}</span>
-    </div>
-  );
-}
+return(
+  <div id="signupDiv">
+    <span id="inner-title">PLEASE SIGN UP </span><br />
+    <input type="text" id="firstName" placeholder="First Name" 
+      onChange={handleSetFirstName} /><br />
+    <input type="text" id="lastName" placeholder="Last Name" 
+      onChange={handleSetLastName} /><br />
+    <input type="text" id="loginEmail" placeholder="Email" 
+      onChange={handleSetEmail} /><br />
+    <input type="text" id="loginName" placeholder="Username" 
+      onChange={handleSetLoginName} /><br />
+    <input type="password" id="loginPassword" placeholder="Password" 
+      onChange={handleSetPassword} /><br/><br/>
+    <button type="button" id="signupButton" className="buttons"
+      onClick={doSignUp}>Sign Up </button><br/><br/>
+    <button type="button" id="signupButton" className="buttons"
+      onClick={goToOnBoard}>Sign Up</button><br />
+    <span id="signupResult">{message}</span>
+  </div>
+);
+};
 
 export default SignUp;
