359ae5798a77b4e95594d3c659ee699d794dce34
RubenDennis <145509659+RubenDennis@users.noreply.github.com>
2025-03-24 13:05:44 -0400

    Adding Home Page

---
+++ frontend/src/components/Home.tsx
@@ -0,0 +1,15 @@
+import React, { useState } from 'react';
+
+function Home()
+{
+    // Functionality
+
+
+  return(
+    <div id="homeDiv">
+      <span id="inner-title">HOME</span><br />
+    </div>
+  );
+};
+
+export default Home;
