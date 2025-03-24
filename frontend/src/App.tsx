a905f4ee897476ae2494f15b6ce7775f75923f25
RubenDennis <145509659+RubenDennis@users.noreply.github.com>
2025-03-24 13:12:43 -0400

    Update App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,6 +5,7 @@ import './App.css';
 import LoginPage from './pages/LoginPage';
 import SignUpPage from './pages/SignUpPage';
 import OnboardingPage from './pages/OnboardingPage';
+import HomePage from './pages/HomePage';
 
 function App() {
 
