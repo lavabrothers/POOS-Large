a48f60ad148dd9731cea17f7008b483a3aa4943a
RubenDennis <145509659+RubenDennis@users.noreply.github.com>
2025-03-24 13:05:17 -0400

    Adding Homa Page

---
+++ frontend/src/pages/HomePage.tsx
@@ -0,0 +1,15 @@
+import PageTitle from '../components/PageTitle.tsx';
+import Home from '../components/Home.tsx';
+
+const HomePage = () =>
+{
+
+    return(
+      <div>
+        <PageTitle />
+        <Home />
+      </div>
+    );
+};
+
+export default HomePage;
