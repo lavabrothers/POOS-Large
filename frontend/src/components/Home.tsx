7af599a750cf2ede3e6a11f86dc2764ed08d6988
RubenDennis <145509659+RubenDennis@users.noreply.github.com>
2025-03-27 21:53:43 -0400

    Partial Chnages to homes

--- frontend/src/components/Home.tsx
+++ frontend/src/components/Home.tsx
@@ -1,15 +1,29 @@
-import React, { useState } from 'react';
+import React from 'react';
 
-function Home()
-{
-    // Functionality
+function Home() {
+  let name: string = "";
+  const userString = localStorage.getItem('user_data');
+  let user: any;
 
+  if (userString !== "" && userString !== null) {
+    user = JSON.parse(userString);
+    name = user.firstName;
+  } else {
+    window.location.href = '/';
+    return <div></div>;
+  }
 
-  return(
+  function goToFavorites(): void {
+    window.location.href = '/favorites';
+  }
+
+  return (
     <div id="homeDiv">
-      <span id="inner-title">HOME</span><br />
+      <span id="inner-title">HOME</span>
+      <h2>Welcome, {name}!</h2>
+      <button onClick={goToFavorites}>Favorites</button>
     </div>
   );
-};
+}
 
 export default Home;
