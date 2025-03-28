133d4440cfaf4c039ea7f5efcd9a979c37ae692e
RubenDennis <145509659+RubenDennis@users.noreply.github.com>
2025-03-27 21:55:17 -0400

    Added favorites route

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,6 +6,7 @@ import LoginPage from './pages/LoginPage';
 import SignUpPage from './pages/SignUpPage';
 import OnboardingPage from './pages/OnboardingPage';
 import HomePage from './pages/HomePage';
+import FavoritesPage from './pages/FavoritesPage';
 
 function App() {
 
@@ -18,6 +19,7 @@ function App() {
         <Route path="/signup" element={<SignUpPage />} />
         <Route path="/onboard" element={<OnboardingPage />} />
         <Route path="/home" element={<HomePage />} />
+        <Route path="/favorites" element={<FavoritesPage />} />
         <Route path="*" element={<Navigate to="/" />} />
       </Routes>
     </Router>
@@ -25,9 +27,3 @@ function App() {
 }
 
 export default App;
-
-
-
-
-
-
