3850a58fd16be044a32effd21740669e1d9971f2
RubenDennis <145509659+RubenDennis@users.noreply.github.com>
2025-03-27 22:30:48 -0400

    Update SignUpPage.tsx

--- frontend/src/pages/SignUpPage.tsx
+++ frontend/src/pages/SignUpPage.tsx
@@ -1,14 +1,17 @@
-import React from 'react';
-import PageTitle from '../components/PageTitle.tsx';
 import SignUp from '../components/SignUp.tsx';
+import Box from '@mui/material/Box';
 
-const SignUpPage = () => {
-  return (
-    <div>
-      <PageTitle />
-      <SignUp />
-    </div>
-  );
+
+const SignUpPage = () => 
+  {
+
+    return(
+      <Box display="flex" flexDirection="column" alignItems="center" justifyContent="center">
+        <Box display="flex" flexDirection="row" alignItems="flex-start" justifyContent="center" gap={4}>
+          <SignUp />
+        </Box>
+      </Box>
+    );
 };
 
 export default SignUpPage;
