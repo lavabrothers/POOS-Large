7deebc01508f400951f77af0c6bf26a5595d8440
RubenDennis <145509659+RubenDennis@users.noreply.github.com>
2025-03-27 21:54:41 -0400

    Create FavoritesPage.tsx

---
+++ frontend/src/pages/FavoritesPage.tsx
@@ -0,0 +1,15 @@
+import PageTitle from '../components/PageTitle.tsx';
+import Favorites from '../components/Favorites.tsx';
+
+const HomePage = () =>
+{
+
+    return(
+      <div>
+        <PageTitle />
+        <Favorites />
+      </div>
+    );
+};
+
+export default HomePage;
