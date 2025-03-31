61fb0568f36946f3edd0b1cb362c72424bb89fa7
mariamauco <mariafmauco@gmail.com>
2025-03-31 16:33:10 -0400

    Update server.js

--- server.js
+++ server.js
@@ -10,10 +10,7 @@ const Favorite = require('./models/Favorite');
 
 // Connect to MongoDB
 //console.log("MONGO_URI:", process.env.MONGO_URI);
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-}).then(() => console.log("Connected to MongoDB"))
+mongoose.connect(process.env.MONGO_URI).then(() => console.log("Connected to MongoDB"))
   .catch(err => console.error("MongoDB connection error:", err));
 
 // Require stock model from separate file
