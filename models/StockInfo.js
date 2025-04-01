1f4b993343b159c9ba823e65b7636ec7a8094c32
mariamauco <mariafmauco@gmail.com>
2025-04-01 19:56:21 -0400

    Create StockInfo.js

---
+++ models/StockInfo.js
@@ -0,0 +1,23 @@
+// models/StockInfo
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+const infoSchema = new Schema({
+  "ticker": { type: String, required: true, unique: true },
+  "company name":{ type: String, required: true },
+  "short name": { type: String }, 
+  "industry": { type: String },
+  "description":  { type: String },
+  "website": {type: String },
+  "logo": { type: String },  // Store the file name of the logo
+  "ceo": { type: String },
+  "exchange": { type: String },
+  "market cap": { type: Number },
+  "sector": { type: String },
+  "tag 1": { type: String },
+  "tag 2": { type: String },
+  "tag 3": { type: String },
+}, { timestamps: true });
+
+const StockInfo = mongoose.model('StockInfo', infoSchema);
+export default Stock;
