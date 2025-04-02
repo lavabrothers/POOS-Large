f3cd525ef044663a5f022cb07a929cdfe42bc518
mariamauco <mariafmauco@gmail.com>
2025-04-01 20:20:05 -0400

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ require('dotenv').config();
 const bcrypt = require('bcrypt'); //for hashing passwords
 const User = require('./models/User'); //for user signup api
 const Favorite = require('./models/Favorite');
-const StockInfo = require('./models/StockInfo');
+//const StockInfo = require('./models/StockInfo');
 
 // Connect to MongoDB
 //console.log("MONGO_URI:", process.env.MONGO_URI);
