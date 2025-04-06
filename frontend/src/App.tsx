8be14db29af115e06dda427c2b15c6c8eb6899d3
RubenDennis <145509659+RubenDennis@users.noreply.github.com>
2025-04-06 18:49:57 -0400

    Update App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { ThemeProvider } from '@mui/material/styles';
+import darkNeonTheme from './darkTheme';
 import './App.css';
 
 import LoginPage from './pages/LoginPage';
@@ -11,17 +13,19 @@ import StockInfoPage from './pages/StockInfoPage';
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<LoginPage />} />
-        <Route path="/signup" element={<SignUpPage />} />
-        <Route path="/onboard" element={<OnboardingPage />} />
-        <Route path="/home" element={<HomePage />} />
-        <Route path="/favorites" element={<FavoritesPage />} />
-        <Route path="/stocks/:symbol" element={<StockInfoPage />} />
-        <Route path="*" element={<Navigate to="/" />} />
-      </Routes>
-    </Router>
+    <ThemeProvider theme={darkNeonTheme}>
+      <Router>
+        <Routes>
+          <Route path="/" element={<LoginPage />} />
+          <Route path="/signup" element={<SignUpPage />} />
+          <Route path="/onboard" element={<OnboardingPage />} />
+          <Route path="/home" element={<HomePage />} />
+          <Route path="/favorites" element={<FavoritesPage />} />
+          <Route path="/stocks/:symbol" element={<StockInfoPage />} />
+          <Route path="*" element={<Navigate to="/" />} />
+        </Routes>
+      </Router>
+    </ThemeProvider>
   );
 }
 
