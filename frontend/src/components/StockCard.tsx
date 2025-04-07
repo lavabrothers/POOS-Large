26563b0dff3d40af911295b76e9a40228add2e5c
Evan Rees <87678009+lavabrothers@users.noreply.github.com>
2025-04-07 19:11:01 -0400

    Update StockCard.tsx

--- frontend/src/components/StockCard.tsx
+++ frontend/src/components/StockCard.tsx
@@ -62,12 +62,9 @@ const StockCard: React.FC<StockCardProps> = ({
     },
     xaxis: {
       categories: categories,
-<<<<<<< HEAD
-      labels:{style:{colors: '#fff'}}
-=======
       labels:{style:{colors: '#fff'}},
       title: { text: 'Fiscal Date Ending', style: {color: '#fff',}},
->>>>>>> 14080280dd0e59fb68b1a9d33bf3c124a6331546
+
     },
     yaxis: {
       title: { text: 'Reported EPS', style: {color: '#fff',}},
